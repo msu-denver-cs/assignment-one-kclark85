0f222b8de7aaea3e3a3038ebfa5ab62c4dc4a894
kclark85 <david@DESKTOP-STG543J.localdomain>
2019-09-16 11:11:46 -0600

    somewhat working version

---
+++ carfactory/db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+parts = Part.create([{ name: 'Automatic Transmission' }, { name: 'Manual Transmission' }, { name: 'v8 engine' }, {  name: 'v6 engine' }])
