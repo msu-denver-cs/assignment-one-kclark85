daaa36dc53632bf6b7fe94f9f8a02bc872693741
kclark85 <david@DESKTOP-STG543J.localdomain>
2019-09-16 07:24:45 -0600

    blowing it up; starting over

--- test/carpartstest/app/controllers/cars_controller.rb
+++ test/carpartstest/app/controllers/cars_controller.rb
@@ -69,6 +69,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:model, :vin, :part)
+      params.require(:car).permit(:model, :make, :vin, :part)
     end
 end
