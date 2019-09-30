ec2e53c137d2225e4046f5d82c7f5d1edbbec9ba
kclark85 <david@DESKTOP-STG543J.localdomain>
2019-09-30 13:20:11 -0600

    added search bar to cars

--- carbuilder/config/routes.rb
+++ carbuilder/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root 'cars#index'
   resources :parts
   resources :makes
-  resources :cars
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
